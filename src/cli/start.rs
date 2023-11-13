b3b8da836bb03dbe99fda1eedfb3ed601b509963
nbari <nbari@tequila.io>
2023-11-13 10:38:17 +0100

    todo examples when using -h

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 
 /// Start the CLI
 pub fn start() -> Result<Action> {
-    let after_help = format!("EXAMPLES: {}", 1);
+    let after_help = String::new();
     let cmd = commands::new(&after_help);
     let matches = cmd.get_matches();
     let action = dispatcher::dispatch(&matches)?;
