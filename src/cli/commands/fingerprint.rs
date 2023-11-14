84e4543b48c9adc964ac8aca6c40cccc62959244
nbari <nbari@tequila.io>
2023-11-14 17:43:00 +0100

    don't allow user new for fingerprint

--- src/cli/commands/fingerprint.rs
+++ src/cli/commands/fingerprint.rs
@@ -1,4 +1,15 @@
-use clap::{Arg, Command};
+use clap::{builder::ValueParser, Arg, Command};
+
+pub fn validator_user() -> ValueParser {
+    ValueParser::from(move |s: &str| -> std::result::Result<String, String> {
+        // Don't allow 'new' as a username
+        if s != "new" {
+            Ok(s.to_owned())
+        } else {
+            Err("Invalid user".to_owned())
+        }
+    })
+}
 
 pub fn subcommand_fingerprint() -> Command {
     Command::new("fingerprint")
@@ -14,6 +25,7 @@ pub fn subcommand_fingerprint() -> Command {
             Arg::new("user")
                 .short('u')
                 .long("user")
-                .help("GitHub username or URL, optional [-k N] where N is the key index"),
+                .help("GitHub username or URL, optional [-k N] where N is the key index")
+                .value_parser(validator_user()),
         )
 }
