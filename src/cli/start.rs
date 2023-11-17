2f84609589167d50c137d29cade2073dd871070c
nbari <nbari@tequila.io>
2023-11-17 21:47:18 +0100

    1.0.5

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -3,8 +3,31 @@ use anyhow::Result;
 
 /// Start the CLI
 pub fn start() -> Result<Action> {
-    let after_help = String::new();
-    let cmd = commands::new(&after_help);
+    let after_help = r#"
+EXAMPLES:
+
+Share a secret:
+
+    echo "secret" | ssh-vault create -u new | pbcopy
+
+Share a secret with a known user in GitHub:
+
+    echo "secret" | ssh-vault create -u alice
+
+Share a secret with Alice using its second key:
+
+    echo "secret" | ssh-vault create -u alice -k 2
+
+View a secret:
+
+    ssh-vault view < secret.txt.vault
+
+Edit a secret:
+
+    ssh-vault edit secret.txt.vault
+
+"#;
+    let cmd = commands::new(after_help);
     let matches = cmd.get_matches();
     let action = dispatcher::dispatch(&matches)?;
     Ok(action)
